fe16359be4dcfe42972d0ec5fb01b90a64ffd35c
Ryan Butler <butlerrc30@gmail.com>
2015-10-13 18:55:06 -0500

    Added files

---
+++ mod_autsonslideshow.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+
+
+// no direct access
+
+defined('_JEXEC') or die('Restricted access');
+
+
+
+ 
+
+require_once ('helper.php');
+
+ 
+require(JModuleHelper::getLayoutPath('mod_autsonslideshow'));
+
+
+
+
+?>
